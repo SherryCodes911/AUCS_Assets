1fd985d1cafd5f3716118cdcc8c5cceddede9ba7
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2024-09-15 10:26:00 +0500

    Uploading Lab04 part2 Task 02 leetcode, there was an error with commit

--- src/DSA_Lab_04_Ahtisham/Lab_Part02_Task_02.java
+++ src/DSA_Lab_04_Ahtisham/Lab_Part02_Task_02.java
@@ -7,11 +7,11 @@ package DSA_Lab_04_Ahtisham;
  * <p>
  * The most significant bit is at the head of the linked list.
  * <p>
- *
- *
+ * <p>
+ * <p>
  * Example 1:
  * <p>
- *
+ * <p>
  * Input: head = [1,0,1]
  * Output: 5
  * Explanation: (101) in base 2 = (5) in base 10
@@ -20,7 +20,7 @@ package DSA_Lab_04_Ahtisham;
  * Input: head = [0]
  * Output: 0
  * <p>
- *
+ * <p>
  * Constraints:
  * <p>
  * The Linked List is not empty.
@@ -29,25 +29,60 @@ package DSA_Lab_04_Ahtisham;
  */
 
 
- // Definition for singly-linked list.
-   class ListNode1 {
-      int val;
-      ListNode1 next;
-      ListNode1() {}
-      ListNode1(int val) { this.val = val; }
-      ListNode1(int val, ListNode1 next) { this.val = val; this.next = next; }
-  }
+// Definition for singly-linked list.
+class ListNode1 {
+    int val;
+    ListNode1 next;
+
+    ListNode1() {
+    }
+
+    ListNode1(int val) {
+        this.val = val;
+    }
+
+    ListNode1(int val, ListNode1 next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution1 {
     public int getDecimalValue(ListNode1 head) {
-    int sum = -1;
+        int sum = 0;
+        int count = -1;
+        ListNode1 temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            count++;
+        }
+        temp = head;
 
+        while (temp != null) {
+            sum += (int) ((Math.pow(2, count)) * temp.val);
+            temp = temp.next;
+            count--;
+        }
 
-    return sum;
+        return sum;
     }
 }
+
 public class Lab_Part02_Task_02 {
     public static void main(String[] args) {
+        ListNode1 head = new ListNode1(1);
+        head.next = new ListNode1(0);
+        head.next.next = new ListNode1(1);
+        head.next.next.next = new ListNode1(0);
+        head.next.next.next.next = new ListNode1(0);
+        head.next.next.next.next.next = new ListNode1(1);
+        head.next.next.next.next.next.next = new ListNode1(0);
+        head.next.next.next.next.next.next.next = new ListNode1(1);
+        head.next.next.next.next.next.next.next.next = new ListNode1(0);
+        head.next.next.next.next.next.next.next.next.next = new ListNode1(1);
+        int sum = new Solution1().getDecimalValue(head);
+        System.out.println(sum);
+
 
     }
 }
