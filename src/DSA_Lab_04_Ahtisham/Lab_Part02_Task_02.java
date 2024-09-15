bed7017e58138fb09f92e65be8d5bf979b28cc53
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2024-09-15 10:24:23 +0500

    Uploading Lab04 part2 Task 02 leetcode

---
+++ src/DSA_Lab_04_Ahtisham/Lab_Part02_Task_02.java
@@ -0,0 +1,53 @@
+package DSA_Lab_04_Ahtisham;
+
+/**
+ * Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+ * <p>
+ * Return the decimal value of the number in the linked list.
+ * <p>
+ * The most significant bit is at the head of the linked list.
+ * <p>
+ *
+ *
+ * Example 1:
+ * <p>
+ *
+ * Input: head = [1,0,1]
+ * Output: 5
+ * Explanation: (101) in base 2 = (5) in base 10
+ * Example 2:
+ * <p>
+ * Input: head = [0]
+ * Output: 0
+ * <p>
+ *
+ * Constraints:
+ * <p>
+ * The Linked List is not empty.
+ * Number of nodes will not exceed 30.
+ * Each node's value is either 0 or 1.
+ */
+
+
+ // Definition for singly-linked list.
+   class ListNode1 {
+      int val;
+      ListNode1 next;
+      ListNode1() {}
+      ListNode1(int val) { this.val = val; }
+      ListNode1(int val, ListNode1 next) { this.val = val; this.next = next; }
+  }
+
+class Solution1 {
+    public int getDecimalValue(ListNode1 head) {
+    int sum = -1;
+
+
+    return sum;
+    }
+}
+public class Lab_Part02_Task_02 {
+    public static void main(String[] args) {
+
+    }
+}
