caea10e692de63f724d2ad03666f9b21df25b241
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2024-09-24 14:34:46 +0500

    Uploading lab5 Part01 Task01, Stack implementation using Array

--- src/DSA_Lab_05_Ahtisham/StackUsingArray.java
+++ src/DSA_Lab_05_Ahtisham/StackUsingArray.java
@@ -1,5 +1,19 @@
 package DSA_Lab_05_Ahtisham;
 
+/**
+ * Task 01: Implement a stack using an array with the following methods:
+ * ● push(int data): Adds an element to the top of the stack.
+ * ● pop(): Removes and returns the top element from the stack.
+ * ● peek(): Returns the top element without removing it.
+ * ● isEmpty(): Checks if the stack is empty.
+ * ● size(): Returns the number of elements in the stack.
+ * Instructions:
+ * ● Define a fixed capacity for your array (for example 5).
+ * ● Handle edge cases such as stack overflow (when the array is full) and stack underflow
+ * (when trying to pop from an empty stack).
+ * ● Write a main method to test all the stack operations by pushing and popping elements,
+ * and printing the stack after each operation.
+ */
 class ArrayStack {
     private final int[] arr;
     private int top;
