ebdc4fa608b8dc40c8977b6d9a365c42be97b484
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2024-09-24 14:06:26 +0500

    Practice file not included for lab purpose

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+    public int[] plusOne(int[] digits) {
+        if (digits[digits.length - 1] != 9) {
+            digits[digits.length - 1]++;
+            return digits;
+        }
+        int[] array = new int[digits.length + 1];
+
+        System.arraycopy(digits, 0, array, 0, digits.length);
+        array[array.length - 2] = 1;
+        array[array.length - 1] = 0;
+
+        return array;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        int[] digits = {9,9};
+//
+//        digits = new Main().plusOne(digits);
+//        for (int digit : digits) {
+//            System.out.print(digit);
+//        }
     }
 }
